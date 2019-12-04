70af8cd47b6450e0d2a272f0346ccaf342916722
DmitryKachalov <rubyzza.dev@gmail.com>
2019-12-04 10:07:38 +0100

    fix

--- right_triangle.rb
+++ right_triangle.rb
@@ -1,15 +1,19 @@
-puts "Введите длину катета A "
-a = gets.chomp.to_f
-puts "Введите длину катета B "
-b = gets.chomp.to_f
-puts "Введите длину гипотенузы C "
-c = gets.chomp.to_f
+puts "Введите длину стороны A "
+side_a = gets.chomp.to_f
+puts "Введите длину стороны B "
+side_b = gets.chomp.to_f
+puts "Введите длину сторны C "
+side_c = gets.chomp.to_f
 
+max_side = [side_a,side_b,side_c].sort
+cathet_1 = max_side.first
+cathet_2 = max_side.fetch(1)
+hypotenuse = max_side.last
 
-if (a**2 + b**2) == c**2
+if (cathet_1**2 + cathet_2**2) == hypotenuse**2
   p "Треугольник прямоугольный"
-elsif a == b && a == c
-  p "Треугольник равнобедренный и равносторонний, но не прямоугольный"
+elsif cathet_1 == cathet_2 && cathet_1 == hypotenuse
+  p "Треугольник равнобедренный и равносторонний"
 else
   p "Треугольник равнобедренный"
 end
